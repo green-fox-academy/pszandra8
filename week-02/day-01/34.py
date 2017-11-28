1c9d97dcf9dbc2743a160b91862679378cd9e7cb
Alexandra Pal <pszandra8@gmail.com>
2017-11-28 17:54:41 +0100

    34. complete

--- week-02/day-01/34.py
+++ week-02/day-01/34.py
@@ -1,10 +1,3 @@
-# Write a program that asks for a number.
-# It would ask this many times to enter an integer,
-# if all the integers are entered, it should print the sum and average of these
-# integers like:
-#
-# Sum: 22, Average: 4.4
-
 number = []
 number.append(int(input("Type in a number: ")))
 number.append(int(input("Type in a number: ")))
@@ -15,7 +8,9 @@ sum = 0
 for i in number:
     sum += i 
 
-print(sum) 
+average = 0
+for n in number:
+    average = sum / n
 
-
-
+print("Sum: " + str(sum))
+print("Average: " + str(average))
