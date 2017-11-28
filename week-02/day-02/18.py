b644337b4c6f258e318c6d810c71e397101f060f
Alexandra Pal <pszandra8@gmail.com>
2017-11-28 15:43:53 +0100

    task 18 is done

--- week-02/day-02/18.py
+++ week-02/day-02/18.py
@@ -1,11 +1,6 @@
-# - Create a variable named `ai`
-#   with the following content: `[3, 4, 5, 6, 7]`
-# - Print the sum of the elements in `ai`
-
 ai = [3, 4, 5, 6, 7]
 result = 0
-n = len(ai)
-for i in range(n):
-    result += ai[n-i]
+for i in ai:
+    result += i 
 
-print(ai) 
+print(result) 
