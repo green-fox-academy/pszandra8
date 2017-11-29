25e567587bd8730e70c23c8992a7563f12971384
Alexandra Pal <pszandra8@gmail.com>
2017-11-29 19:48:16 +0100

    Task 6. has been finished.

--- week-02/day-03/Lists/6.py
+++ week-02/day-03/Lists/6.py
@@ -1,12 +1,10 @@
-
-# Check if list contains all of the following elements: 4,8,12,16
-# Create a function that accepts list_of_numbers as an input
-# it should return "True" if it contains all, otherwise "False"
-
 list_of_numbers = [2, 4, 6, 8, 10, 12, 14, 16]
 
-for n in list_of_numbers:
-    if n == 4 and n == 8 and n == 12 and n == 16:
-        print("All in it")
-    else:
-        print("Not all of them are part of the list")
+def in_it(list_of_numbers):
+    for n in list_of_numbers:
+        if n == 4 and n == 8 and n == 12 and n == 16:
+            return(True)
+        else:
+            return(False)
+
+in_it(list_of_numbers)
