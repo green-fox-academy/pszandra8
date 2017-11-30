4318ef24777d671709e3048a50030318939ea25d
Alexandra Pal <pszandra8@gmail.com>
2017-11-30 12:34:19 +0100

    Comment has been removed from the code

--- week-02/day-03/Dictonaries/5.py
+++ week-02/day-03/Dictonaries/5.py
@@ -1,18 +1,3 @@
-# Create a function that prints the ingredient list of dictionaries to the console in the following format:
-#
-# +--------------------+---------------+----------+
-# | Ingredient         | Needs cooling | In stock |
-# +--------------------+---------------+----------+
-# | vodka              | Yes           | 1        |
-# | coffee_liqueur     | Yes           | -        |
-# | fresh_cream        | Yes           | 1        |
-# | captain_morgan_rum | Yes           | 2        |
-# | mint_leaves        | No            | -        |
-# +--------------------+---------------+----------+
-#
-# OPTIONAL:
-# The frist columns should be automatically as wide as the longest key
-
 ingredients = [
 	{ "name": "vodka", "in_stock": 1, "needs_cooling": True },
 	{ "name": "coffee_liqueur", "in_stock": 0, "needs_cooling": True },
