514300827e2318dcc44bab89ab4ccb811e722f39
Alexandra Pal <pszandra8@gmail.com>
2017-12-06 17:53:33 +0100

    17 is half done

--- week-03/day-03/16.py
+++ week-03/day-03/16.py
@@ -1,14 +1,9 @@
-
 from tkinter import *
 
 root = Tk()
 
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
-
-# divide the canvas into 4 equal parts
-# and repeat this pattern in each quarter:
-# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
 def drawing(x, y):
     for i in range(15):
