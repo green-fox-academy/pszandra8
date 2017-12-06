3cbd9c4311621c43fb74a12b9a149fd0bd98a7dd
Alexandra Pal <pszandra8@gmail.com>
2017-12-06 10:46:36 +0100

    Exercise 5 is done

---
+++ week-03/day-03/4.py
@@ -0,0 +1,17 @@
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def drawing(x, y):
+    canvas.create_line(x, y, 150, 150)
+
+drawing(10, 10)
+drawing(280, 10)
+drawing (48, 30)
+
+root.mainloop()
+
