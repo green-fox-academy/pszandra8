dd28c413a0f361810687ef465a42c377a5863a12
Alexandra Pal <pszandra8@gmail.com>
2017-12-06 11:05:17 +0100

    8 is done

---
+++ week-03/day-03/7.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# draw four different size and color rectangles.
+canvas.create_rectangle(90, 90, 210, 210, fill = "yellow")
+canvas.create_rectangle(110, 110, 190, 190, fill = "red")
+canvas.create_rectangle(130, 130, 170, 170, fill = "green")
+canvas.create_rectangle(145, 145, 155, 155, fill = "gold")
+
+root.mainloop()
