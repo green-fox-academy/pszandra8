1fad078e45d51937830431189628df564dd27653
Alexandra Pal <pszandra8@gmail.com>
2017-12-08 18:34:07 +0100

    Modified

--- week-03/day-03/10.py
+++ week-03/day-03/10.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas = Canvas(root, width = "300", height = "300")
 canvas.pack()
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple", "violet"]
@@ -11,7 +11,9 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple", "violet"]
 def drawing(x, color):
     for i in colors:
         x -= 20
-        canvas.create_rectangle(150 - x, 150 - x, 150 + x, 150 + x, fill = i)
+        canvas.create_rectangle(
+            150 - x, 150 - x, 150 + x, 150 + x, fill = i
+            )
 
 drawing(150, colors)
 
