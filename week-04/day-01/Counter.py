86a49c3afca2beb4c92511a593b75343e84009a6
Alexandra Pal <pszandra8@gmail.com>
2017-12-11 19:51:38 +0100

    Counter is done

--- week-04/day-01/Counter.py
+++ week-04/day-01/Counter.py
@@ -1,14 +1,20 @@
-Create Counter class
-which has an integer field value
-when creating it should have a default value 0 or we can specify it when creating
-we can add(number) to this counter another whole number
-or we can add() without parameters just increasing the counter's value by one
-and we can get() the current value
-also we can reset() the value to the initial value
-Check if everything is working fine with the proper test
-Download test_counter.py and place it next to your solution
-Run the test file as a usual python program
-
 class Counter(object):
-    def __init__(self, field = 0):
-        self.field = field
+    value_initial = 0
+    def __init__(self, value = 0):
+        self.value = int(value)
+        Counter.value_initial = value
+
+    def add(self):
+        self.value += 1
+
+    def get(self, index = None):
+        return self.value
+    
+    def reset(self):
+        self.value = Counter.value_initial
+
+new_object = Counter(100)
+new_object.add()
+print(new_object.get())
+new_object.reset()
+print(new_object.get())
