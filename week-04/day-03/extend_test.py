cfebafa6995cce336c1330f16b73b66b027d1cad
Alexandra Pal <pszandra8@gmail.com>
2017-12-13 16:19:53 +0100

    Extension has been completed

---
+++ week-04/day-03/extend_test.py
@@ -0,0 +1,38 @@
+import unittest
+import extend
+
+class TestExtend(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_add_2_and_3_is_5(self):
+        self.assertEqual(extend.add(3, 3), 6)
+
+    def test_add_4_and_1_is_5(self):
+        self.assertEqual(extend.add(4, 6), 10)
+
+    def test_max_of_three_first(self):
+        self.assertEqual(extend.max_of_three(4, 5, 3), 5)
+
+    def test_max_of_three_third(self):
+        self.assertEqual(extend.max_of_three(3, 9, 5), 9)
+
+    def test_median_four(self):
+        self.assertEqual(extend.median([5,7,3,5]), 5)
+
+    def test_median_five(self):
+        self.assertEqual(extend.median([1,3,2,4,5]), 3)
+
+    def test_is_vovel_a(self):
+        self.assertFalse(extend.is_vovel('b'))
+
+    def test_is_vovel_u(self):
+        self.assertTrue(extend.is_vovel('u'))
+
+    def test_translate_bemutatkozik(self):
+        self.assertEqual(extend.translate('bemutatkozik'), 'bevemuvutavatkovozivik')
+
+    def test_translate_kolbice(self):
+        self.assertEqual(extend.translate('alma'), 'avalmava')
+
+if __name__ == '__main__':
+    unittest.main()
