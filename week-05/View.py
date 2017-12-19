5617a53d2f6d3fbc8a4579e5bb570511825979d7
Alexandra Pal <pszandra8@gmail.com>
2017-12-19 15:00:43 +0100

    Hero can move

--- week-05/View.py
+++ week-05/View.py
@@ -1,5 +1,7 @@
 from tkinter import *
-from PIL import ImageTk, Image
+
+root = Tk()
+canvas = Canvas(root, width = "720", height = "720")
 
 class Map(object):
 
@@ -15,74 +17,84 @@ class Map(object):
         [0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
         [0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
         [0, 0, 0, 1, 0, 1, 1, 0, 1, 0]]
-        self.root = Tk()
-        self.canvas = Canvas(self.root, width = "720", height = "720")
-        self.canvas.pack()
-        self.actual_floor = ""
-        self.actual_wall = ""
-
+        self.floor = PhotoImage(file = "floor.gif")
+        self.wall = PhotoImage(file = "wall.gif")
 
     def drawer(self):
-        floor = Image.open("floor.gif")
-        floor_2 = ImageTk.PhotoImage(floor)
-        self.actual_floor = floor_2
-        wall = Image.open("wall.gif")
-        wall_2 = ImageTk.PhotoImage(wall)
-        self.actual_wall = wall_2
         for i in range(len(self.area)):
             for j in range(len(self.area[i])):
                 y = j * 72
                 x = i * 72
                 if self.area[j][i] == 0:
-                    image = self.canvas.create_image(x, y, anchor = NW, image = self.actual_floor)
+                    image = canvas.create_image(x, y, anchor = NW, image = self.floor)
                 else:
-                    image = self.canvas.create_image(x, y, anchor = NW, image = self.actual_wall)
-        self.canvas.update()
+                    image = canvas.create_image(x, y, anchor = NW, image = self.wall)
 
-# class Hero(object):
-#     def __init__(self):
-#         self.x = 36
-#         self.y = 36
-#         self.down = PhotoImage(file = "hero-down.gif")
+class Hero(object):
+    def __init__(self, canvas):
+        self.hx = 0
+        self.hy = 0
+        self.canvas = canvas
+        self.down = PhotoImage(file = "hero-down.gif")
+        self.up = PhotoImage(file = "hero-up.gif")
+        self.right = PhotoImage(file = "hero-right.gif")
+        self.left = PhotoImage(file = "hero-left.gif")
 
-#     def move(self, e):
-#         self.up = PhotoImage(file = "hero-up.gif")
-#         self.right = PhotoImage(file = "hero-right.gif")
-#         self.left = PhotoImage(file = "hero-left.gif")
-#         self.down = PhotoImage(file = "hero-down.gif")
-#         self.hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.down)
-#         if e.move == "Up":
-#             if self.y >= 72:
-#                y -= 72
-#                hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.up)
-#             else:
-#                 hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.up)
+    def move_up(self):
+        self.canvas.create_image(self.hx, self.hy, anchor = NW, image = self.up)
 
-#         elif e.move == "Down":
-#             if self.y <= 720:
-#                 y += 72
-#                 hero = canvas.create_image(self.x, self.y + 72, anchor = NW, image = self.down)
-#             else:
-#                 hero = canvas.create_image(self.x, self.y + 72, anchor = NW, image = self.down)
+    def move_right(self):
+        self.canvas.create_image(self.hx, self.hy, anchor = NW, image = self.right)
+    
+    def move_left(self):
+        self.canvas.create_image(self.hx, self.hy, anchor = NW, image = self.left)
 
-#         elif e.move == "Left":
-#             if self.x >= 72:
-#                 x -= 72
-#                 hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.left)
-#             else:
-#                 hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.left)
-
-#         elif e.move == "Right":
-#             if self.x <= 720:
-#                 x += 72
-#                 hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.right)
-#             else:
-#                 hero = canvas.create_image(self.x, self.y, anchor = NW, image = self.right)
-#          self.canvas.update()
+    def move_down(self):
+        canvas.create_image(self.hx, self.hy, anchor = NW, image = self.down)
 
 map = Map()
-#hero = Hero()
 map.drawer()
-#canvas.bind("<KeyPress>", hero.move)
-#canvas.focus_set()
-map.root.mainloop()
+
+hero = Hero(canvas)
+hero.move_down()
+
+def move(e):
+    if e.keysym == "Up":
+        if hero.hy >= 72:
+            map.drawer()
+            hero.hy -= 72
+            hero.move_up()
+        else:
+            map.drawer()
+            hero.move_up()
+
+    elif e.keysym == "Down":
+        if hero.hy <= 640:
+            map.drawer()
+            hero.hy += 72
+            hero.move_down()  
+        else:
+            map.drawer()
+            hero.move_down()
+    elif e.keysym == "Left":
+        if hero.hx >= 72:
+            map.drawer()
+            hero.hx -= 72
+            hero.move_left()
+        else:
+            map.drawer()
+            hero.move_left()
+
+    elif e.keysym == "Right":
+        if hero.hx <= 640:
+            map.drawer()
+            hero.hx += 72
+            hero.move_right()
+        else:
+            map.drawer()
+            hero.move_right()
+
+canvas.bind("<KeyPress>", move)
+canvas.pack()
+canvas.focus_set()
+root.mainloop()
