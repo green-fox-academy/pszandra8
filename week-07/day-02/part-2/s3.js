8970d61c80be4a651ab2cb44bf6721dcd9ceb664
Alexandra Pal <pszandra8@gmail.com>
2018-01-09 15:36:05 +0100

    s3 done

--- week-07/day-02/part-2/s3.js
+++ week-07/day-02/part-2/s3.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let url = "https//www.reddit.com/r/nevertellmethebots";
-url.indexOf('');
+url = url.replace("bots", "odds");
+url = url.substr(0, 7) + ':' + url.substr(7, 37);
 
 console.log(url);
