fd7fcab29587263dce58f8fe6592f47ea3087a62
Alexandra Pal <pszandra8@gmail.com>
2018-01-09 17:30:52 +0100

    Matrix has been rethought

--- week-07/day-02/part-1/Matrix.js
+++ week-07/day-02/part-1/Matrix.js
@@ -1,6 +1,17 @@
 'use strict';
 
+let matrix = [[]];
 let size = 4;
-let myMatrix = [];
 
-console.log(myMatrix);
+for (let i = 0; i <= matrix.length; i++) {
+  for (let j = 0; j <= matrix.lenth; j ++) {
+    if (i + j === size - 1) {
+      matrix[i][j] = 1;
+    }
+    else {
+      matrix[i][j] = 0;
+    }
+  }
+}
+
+console.log(matrix);
