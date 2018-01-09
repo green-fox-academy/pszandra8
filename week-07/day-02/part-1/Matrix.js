f5ed8a02620b9eefb257dd068e1ad49b202b5167
Alexandra Pal <pszandra8@gmail.com>
2018-01-09 17:32:46 +0100

    Matrix has been rethought

--- week-07/day-02/part-1/Matrix.js
+++ week-07/day-02/part-1/Matrix.js
@@ -4,7 +4,7 @@ let matrix = [[]];
 let size = 4;
 
 for (let i = 0; i <= matrix.length; i++) {
-  for (let j = 0; j <= matrix.lenth; j ++) {
+  for (let j = 0; j <= size.length; j ++) {
     if (i + j === size - 1) {
       matrix[i][j] = 1;
     }
