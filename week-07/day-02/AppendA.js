c94a7633e036018e2308ec2f80b04f062107d4bd
Alexandra Pal <pszandra8@gmail.com>
2018-01-09 10:37:57 +0100

    DoubleItems complete

---
+++ week-07/day-02/AppendA.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let nimals = ["kuty", "macsk", "cic"];
+
+function adder(element) {
+    console.log(element + 'a');
+}
+nimals.forEach(adder);
---
+++ week-07/day-02/DoubleItems.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let ag = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+function double(element) {
+    console.log(element.repeat(2));
+}
+
+ag.forEach(double);
