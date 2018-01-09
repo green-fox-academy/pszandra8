866c73976ef49626852ae4cbc48b50855a7ae60c
Alexandra Pal <pszandra8@gmail.com>
2018-01-09 17:20:19 +0100

    Matrix is incomplete

--- week-07/day-02/part-1/Matrix.js
+++ week-07/day-02/part-1/Matrix.js
@@ -1,18 +1,6 @@
 'use strict';
 
-let matrix = [];
 let size = 4;
-for(var i = 0; i < size; i++) {
-  let empty_list = []
-  for (let j = 0; j < size; j++) {
-    if (i + j == size - 1){
-      empty_list[j] = 1;
-    }
-    else {
-      empty_list[j] = 0;
-    }
-  }
-  matrix[i] = empty_list;
-}
+let myMatrix = [];
 
-console.log(matrix);
+console.log(myMatrix);
