17a0f47971409442ecd0a979bafa045dcf6f795f
Alexandra Pal <pszandra8@gmail.com>
2018-01-15 17:13:46 +0100

    Animal done

--- week-08/Animal.js
+++ week-08/Animal.js
@@ -1,5 +1,4 @@
-function Animal(type) {
-  this.type = type,
+function Animal() {
   this.hunger = 5,
   this.thirst = 5,
   this.eat = function() {
@@ -26,31 +25,64 @@ function Farm(slots) {
 
   this.breed = function() {
     if (this.slots !== 0) {
-      let Sheep = new Animal('sheep');
-      this.animals.push(Sheep);
+      this.animals.push(new Animal());
       this.slots -= 1;
     }
+  }
 
   this.slaughter = function() {
     let minHunIndex = 0;
-    for (i = 0; i <= animals; i++) {
-      if (animals[i].hunger < minHunIndex) {
+    for (i = 0; i <= this.animals; i++) {
+      if (this.animals[i].hunger < minHunIndex) {
         minHunIndex = i;
       }
       }
       this.animals.splice(minHunIndex, 1);
       }
+
+  this.progress = function() {
+    for (i = 0; i <= this.animals.length; i++) {
+      let chance = Math.floor(Math.random() * (3 - 1 + 1));
+      if (chance === 1) {
+        this.animals[i].eat();
+      }
+      else if (chance === 2) {
+        this.animals[i].drink();
+      }
+      else if (chance === 3) {
+        this.animals[i].play();
+      }
+      }
     }
+    this.breed();
+    this.slaughter();
+
+  this.reports = function() {
+    let animalNum = 0;
+    for (i = 0; i <= this.animals; i++) {
+      console.log(i);
+      animalNum = i;
+    if (animalNum === 0) {
+      console.log('bankrupt');
+    }
+    else if (animalNum > 0 && this.slots > animalNum) {
+      console.log('okay');
+    }
+    else if (animalNum === this.slots) {
+      console.log('full')
+    }
+  }
+}
 }
 
 const SheepFarm = new Farm(20);
+SheepFarm.breed();
+SheepFarm.breed();
+SheepFarm.breed();
 
+console.log(SheepFarm.animals);
 
-console.log(SheepFarm.animals); // Should log 20 Animal objects
+console.log(SheepFarm.animals);
 
-// const button = document.querySelector('button');
-
-// Add a click event to the button and call 'progress'
-
-// The progress function should log the following to the console:
-//  - The farm has 20 living animals, we are full
+const button = document.querySelector('button');
+button.addEventListener('click', SheepFarm.progress());
