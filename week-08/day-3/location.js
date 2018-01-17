10104aa098544b6aa1596c4fb9ed6d5646081dbd
Alexandra Pal <pszandra8@gmail.com>
2018-01-17 17:34:25 +0100

    just the files for the last 2 exercises

---
+++ week-08/day-3/location.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=Budapest?luOXpshuQ5mshWFHf7PlZCHfIBZSp19U6R', true);
+httpRequest.send();
+httpRequest.onload = function () {
+  if (httpRequest.status >= 200 && httpRequest.status < 400) {
+      let text = JSON.parse(httpRequest.responseText);
+      console.log(text);
+  } else {
+      console.log('error');
+  }
+};
