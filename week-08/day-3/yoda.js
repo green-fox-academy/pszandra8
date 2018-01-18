be1a1844cda424d5d5eb9979751855f3426cf827
Alexandra Pal <pszandra8@gmail.com>
2018-01-18 14:30:07 +0100

    html file half done

--- week-08/day-3/yoda.js
+++ week-08/day-3/yoda.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=New+York?FuYuOBFjXGmsh2XlcMWjliKzRELNp1niS6RjsnjW5ekCzXSEOb', true);
+httpRequest.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php', true);
 httpRequest.send();
 httpRequest.onload = function () {
   if (httpRequest.status >= 200 && httpRequest.status < 400) {
