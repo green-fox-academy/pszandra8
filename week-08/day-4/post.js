8b11009ed1addeacb79099cedd5f2d0fcee005f3
Alexandra Pal <pszandra8@gmail.com>
2018-01-20 15:58:38 +0100

    Posting function done

--- week-08/day-4/post.js
+++ week-08/day-4/post.js
@@ -1,27 +1,24 @@
 'use strict'
 
+
+
 let submitButton = document.querySelector('.submit');
-let title = document.querySelector('.title');
-let url = document.querySelector('.url');
+let postTitle = document.querySelector('.title');
+let postUrl = document.querySelector('.url');
+
 
 let alertOfEmptyField = function () {
-  if (title.value === null || title.value === ' ' || title.value === '') {
+  if (postTitle.value === null || postTitle.value === ' ' || postTitle.value === '') {
     alert('Please kindly fill out the "title" field for posting!');
     }
     else {
-      post(string);
-      console.log('ok');
+      posting();
     }
   }
 
 submitButton.addEventListener('click', alertOfEmptyField);
 
-let string = {
-  'title': title.value,
-  'url': url.value
-}
-
-function post(string){
+function posting(){
   let httpRequest = new XMLHttpRequest();
 
   httpRequest.open('POST', 'http://secure-reddit.herokuapp.com/simple/posts');
@@ -29,12 +26,12 @@ function post(string){
   httpRequest.setRequestHeader('Content-type', 'application/json');
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === httpRequest.DONE && httpRequest.status === 200) {
-      console.log('okay');
+      console.log('HTTP REQUEST SUCCESSFUL');
     }
     else {
       console.log('could not complete the request');
     }
     }
-    httpRequest.send(string);
-    string = JSON.stringify(string);
+    let post = '{"title": "' + postTitle.value + '","url": "' + postUrl.value + '"};';
+    httpRequest.send(post);
 }
