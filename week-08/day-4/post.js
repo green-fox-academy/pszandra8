d628f48fb585a1d0820f9c73bda154402271e867
Alexandra Pal <pszandra8@gmail.com>
2018-01-20 13:34:10 +0100

    Posting function half done

--- week-08/day-4/post.js
+++ week-08/day-4/post.js
@@ -1,12 +1,40 @@
 'use strict'
 
 let submitButton = document.querySelector('.submit');
-let label = document.querySelector('label');
+let title = document.querySelector('.title');
+let url = document.querySelector('.url');
 
 let alertOfEmptyField = function () {
-  if (label.textContent === null || label.textContent === ' ' || label.textContent === '') {
-    alert('Please kindly fill out the title and the URL fields!');
+  if (title.value === null || title.value === ' ' || title.value === '') {
+    alert('Please kindly fill out the "title" field for posting!');
+    }
+    else {
+      post(string);
+      console.log('ok');
+    }
   }
-}
 
 submitButton.addEventListener('click', alertOfEmptyField);
+
+let string = {
+  'title': title.value,
+  'url': url.value
+}
+
+function post(string){
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.open('POST', 'http://secure-reddit.herokuapp.com/simple/posts');
+  httpRequest.setRequestHeader('Accept', 'application/json');
+  httpRequest.setRequestHeader('Content-type', 'application/json');
+  httpRequest.send(string);
+  string = JSON.stringify(string);
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === httpRequest.DONE && httpRequest.status === 200) {
+      console.log('okay');
+    }
+    else {
+      console.log('could not complete the request');
+    }
+    }
+}
