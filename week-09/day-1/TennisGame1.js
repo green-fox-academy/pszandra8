9d39f3a0ca9e3d04636c1e11ea5e65deca1889b4
Alexandra Pal <pszandra8@gmail.com>
2018-01-22 15:53:29 +0100

    TennisGame1 indentation fixed

--- week-09/day-1/TennisGame1.js
+++ week-09/day-1/TennisGame1.js
@@ -1,35 +1,35 @@
 'use strict'
 
 let TennisGame1 = function(player1Name, player2Name) {
-    this.scoreOfPlayer1 = 0;
-    this.scoreOfPlayer2 = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+  this.scoreOfPlayer1 = 0;
+  this.scoreOfPlayer2 = 0;
+  this.player1Name = player1Name;
+  this.player2Name = player2Name;
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
-        this.scoreOfPlayer1 += 1;
-    else
-        this.scoreOfPlayer2 += 1;
+  if (playerName === "player1")
+    this.scoreOfPlayer1 += 1;
+  else
+    this.scoreOfPlayer2 += 1;
 };
 
 TennisGame1.prototype.getScore = function() {
-    let score = "";
-    let tempScore = 0;
-    if (this.scoreOfPlayer1 === this.scoreOfPlayer2) {
-        if (this.scoreOfPlayer1 === 0) {
-            score = "Love-All";
-        }
-        else if (this.scoreOfPlayer1 === 1) {
-            score = "Fifteen-All";
-        }
-        else if (this.scoreOfPlayer1 === 2) {
-            score = "Thirty-All";
-        }
-        else {
-            score = "Deuce";
-        }
+  let score = "";
+  let tempScore = 0;
+  if (this.scoreOfPlayer1 === this.scoreOfPlayer2) {
+    if (this.scoreOfPlayer1 === 0) {
+      score = "Love-All";
+    }
+    else if (this.scoreOfPlayer1 === 1) {
+      score = "Fifteen-All";
+    }
+    else if (this.scoreOfPlayer1 === 2) {
+      score = "Thirty-All";
+    }
+    else {
+      score = "Deuce";
+    }
     } else if (this.scoreOfPlayer1 >= 4 || this.scoreOfPlayer2 >= 4) {
         let minusResult = this.scoreOfPlayer1 - this.scoreOfPlayer2;
         if (minusResult === 1) score = "Advantage player1";
@@ -38,26 +38,26 @@ TennisGame1.prototype.getScore = function() {
         else score = "Win for player2";
     } else {
         for (let i = 1; i < 3; i++) {
-            if (i === 1) {
-                tempScore = this.scoreOfPlayer1;
+          if (i === 1) {
+            tempScore = this.scoreOfPlayer1;
             }
-            else {
-                score += "-";
-                tempScore = this.scoreOfPlayer2;
+          else {
+            score += "-";
+            tempScore = this.scoreOfPlayer2;
             }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
+          switch (tempScore) {
+            case 0:
+              score += "Love";
+                break;
+            case 1:
+              score += "Fifteen";
+                break;
+            case 2:
+              score += "Thirty";
+                break;
+            case 3:
+              score += "Forty";
+                break;
             }
         }
     }
@@ -65,5 +65,5 @@ TennisGame1.prototype.getScore = function() {
 };
 
 if (typeof window === "undefined") {
-    module.exports = TennisGame1;
+  module.exports = TennisGame1;
 }
