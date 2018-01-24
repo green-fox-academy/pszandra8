d9cd8578ab48c783a8dbfdf1b0057e4ba1622e3b
Alexandra Pal <pszandra8@gmail.com>
2018-01-24 15:12:12 +0100

    Anagram

--- week-09/day-3/Anagram.js
+++ week-09/day-3/Anagram.js
@@ -1,9 +1,12 @@
 'use strict'
 
 let isAnagram = function (one, two) {
-  let listOne = one.sort();
-  let listTwo = two.sort();
-  if (listOne === listTwo) {
+  let listOne = one.toLowerCase().split('').sort().join('');
+  let listTwo = two.toLowerCase().split('').sort().join('');
+  if (typeof one !== typeof two) {
+    false;
+  }
+  else if (listOne === listTwo) {
     return true;
   } else {
     return false;
--- week-09/day-3/Anagram_test.js
+++ week-09/day-3/Anagram_test.js
@@ -3,9 +3,17 @@
 var test = require('tape');
 var isAnagram = require('./Anagram.js');
 
-test('anagram', function (t) {
-  var actual = isAnagram(['lorem'], ['ipsum']);
+test('not equal strings', function (t) {
+  var actual = isAnagram('lorem', 'ipsum');
   var expected = false;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('equal strings', function (t) {
+  var actual = isAnagram('god', 'dog');
+  var expected = true;
 
   t.equal(actual, expected);
   t.end();
