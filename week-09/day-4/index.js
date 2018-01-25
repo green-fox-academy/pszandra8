2e803cc5d8f148ae5254f3878bd058371a6b2a39
Alexandra Pal <pszandra8@gmail.com>
2018-01-25 15:56:34 +0100

    appenda done

--- week-09/day-4/index.js
+++ week-09/day-4/index.js
@@ -29,6 +29,63 @@ app.get('/doubling', function (req, res) {
   }
 });
 
+app.get('/greeter', function (req, res) {
+  const name = req.query.name;
+  const title = req.query.title;
+  if (name === undefined) {
+    res.json(
+      {
+        error: 'Please provide a name!'
+      }
+    );
+  } else if (title === undefined) {
+    res.json(
+      {
+        error: 'Please provide a title!'
+      }
+    );
+  }
+  else {
+    res.json(
+      {
+        welcome_message: `Oh, hi there ${name}, my dear ${title}!`
+      }
+    );
+  }
+});
+
+app.get('/appenda/:appendable', function (req, res) {
+  const appendable = req.params.appendable;
+  if (appendable !== undefined) {
+  res.json(
+    {
+      appended: `${appendable}a`
+    }
+  );
+}
+  else {
+    {
+      res.status(404);
+    }
+  }
+});
+
+app.post('/appenda/:appendable', function (req, res) {
+  const appendable = req.params.appendable;
+  if (appendable !== undefined) {
+  res.json(
+    {
+      appended: `${appendable}a`
+    }
+  );
+}
+  else {
+    {
+      res.status(404);
+    }
+  }
+});
+
 app.listen(8080, function () {
   console.log('app is running');
 });
