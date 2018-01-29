6c1ab61075ab49240361ba19b4380d9c93a6d8c0
Alexandra Pal <pszandra8@gmail.com>
2018-01-29 19:03:12 +0100

    click works for one part

--- week-10/foxplayer/fox_frontend.js
+++ week-10/foxplayer/fox_frontend.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let playList = document.querySelectorAll('.play');
+playList.forEach(function(element) {
+  element.addEventListener('click', function(){
+    playList.forEach(function(e){
+      e.classList.remove('selected_item');
+    })
+    element.classList.add('selected_item');
+  })
+})
